a5f1e399935bdda7b9c90d2e5c221addb27bbbe1
david <lexuszhi1990@gmail.com>
2018-07-28 00:03:22 +0800

    add loss

---
+++ src/loss.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def svm_loss(x, y):
+  """
+  Computes the loss and gradient using for multiclass SVM classification.
+
+  Inputs:
+  - x: Input data, of shape (N, C) where x[i, j] is the score for the jth class
+    for the ith input.
+  - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
+    0 <= y[i] < C
+
+  Returns a tuple of:
+  - loss: Scalar giving the loss
+  - dx: Gradient of the loss with respect to x
+  """
+  N = x.shape[0]
+  correct_class_scores = x[np.arange(N), y]
+  margins = np.maximum(0, x - correct_class_scores[:, np.newaxis] + 1.0)
+  margins[np.arange(N), y] = 0
+  loss = np.sum(margins) / N
+  num_pos = np.sum(margins > 0, axis=1)
+  dx = np.zeros_like(x)
+  dx[margins > 0] = 1
+  dx[np.arange(N), y] -= num_pos
+  dx /= N
+  return loss, dx
+
+
+def softmax_loss(y_hat, y):
+    """
+    Computes the loss and gradient for softmax classification.
+
+    Inputs:
+    - y_hat: Input data, of shape (N, C) where x[i, j] is the score for the jth class
+      for the ith input.
+    - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
+      0 <= y[i] < C
+
+    Returns a tuple of:
+    - loss: Scalar giving the loss
+    - dx: Gradient of the loss with respect to x
+    """
+    probs = np.exp(y_hat - np.max(y_hat, axis=1, keepdims=True))
+    probs /= np.sum(probs, axis=1, keepdims=True)
+    batch_size = y_hat.shape[0]
+    loss = -np.sum(np.log(probs[np.arange(batch_size), y] + 1e-100)) / batch_size
+    dx = probs.copy()
+    dx[np.arange(batch_size), y] -= 1
+    dx /= batch_size
+    return loss, dx
