3289f786a3ab9e83755abfd04ec89e0b317bef7d
david <lexuszhi1990@gmail.com>
2018-07-29 02:14:01 +0800

    update code

--- src/configuration.py
+++ src/configuration.py
@@ -3,13 +3,14 @@ from collections import defaultdict
 cfg_list = defaultdict(set)
 
 cfg_list['mlp'] = {
-    'workspace': './ckpt/mlp-v2',
+    'workspace': './ckpt/mlp-v3',
+    'dataset_name': 'mnist',
     'symbol': 'mlp',
-    'batch_size': 5000,
+    'batch_size': 10000,
     'max_epoch': 100,
     'milestones': [ i*10 for i in range(1, 100//10) ],
-    'gamma': 0.75,
-    'base_lr': 1e-2,
+    'gamma': 0.6,
+    'base_lr': 5e-2,
     'momentum': 0.9,
-    'reg': 1e-3,
+    'reg': 5e-3,
 }
