02e8d566bd4e2d6efe5a5932f9a1e886b0b4f5e3
david <lexuszhi1990@gmail.com>
2018-07-28 19:34:48 +0800

    add args

---
+++ src/args.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description='neural networks based py')
+
+    # choose config for training
+    parser.add_argument('--config_id', default='mlp', type=str, help='config ID')
+
+    # choose ckpt_path for evaluating
+    parser.add_argument('--ckpt_path', default='./data/trained_model/mlp-99.json', type=str, help='params path to load base model')
+
+    args = parser.parse_args()
+    return args
