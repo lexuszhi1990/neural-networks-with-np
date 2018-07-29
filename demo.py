f56d7728221dff5b4163b3147900d315b6ac4f6f
david <lexuszhi1990@gmail.com>
2018-07-29 14:22:40 +0800

    add demo.py

---
+++ demo.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import numpy as np
+from src.symbol import get_symbol
+from src.logger import setup_logger
+from src.args import get_args
+
+from src.configuration import cfg_list
+
+
+def demo(symbol_name, params_path, img_path):
+    pass
