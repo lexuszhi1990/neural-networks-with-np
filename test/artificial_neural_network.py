1f3b6d33a47fd6a6d0f87a583c79d24592df03af
david <lexuszhi1990@gmail.com>
2018-07-30 15:57:29 +0800

    udpate test

--- test/artificial_neural_network.py
+++ test/artificial_neural_network.py
@@ -28,20 +28,26 @@ y = np.array([
 '''
 
 w = np.random.randn(3, 1)
+b = 0
 
 for e in range(10000):
-    output = np.dot(x, w)
+    output = np.dot(x, w) + b
     y_hat = sigmoid(output)
     loss = 0.5 * (y_hat - y) ** 2 / len(y_hat)
 
     d_loss = (y_hat - y)
     d_y_hat = d_sigmoid(output)
 
+    d_out = d_loss * d_y_hat
+
     dw = x.T.dot(d_loss * d_y_hat)
-    w -= 0.8 * dw
+    db = np.sum(d_out, axis=0)
+    w -= 0.05 * dw
+    b -= 0.05 * db
 
+    dx = np.dot(d_out, w.T)
 
-output = np.dot(x, w)
+output = np.dot(x, w) + b
 y_hat = sigmoid(output)
 print(np.abs(y_hat-y))
 import pdb
